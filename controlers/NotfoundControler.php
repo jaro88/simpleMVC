9a5b0a1094cdbe00217d7722d65b75f3834941e9
jaro88 <kokoruda.j@gmail.com>
2016-03-09 13:42:37 +0100

    vytvorenie jednoducheho MVC frameworku

--- controlers/NotfoundControler.php
+++ controlers/NotfoundControler.php
@@ -5,7 +5,7 @@ class NotfoundControler extends Controler
 {
     public function execute($parametry)
     {
-		// Hlavička požadavku
+		// Hlavička požadavku 
 		header("HTTP/1.0 404 Not Found");
 		// Nastavení šablony
 		$this->template = 'notfound.tpl';
